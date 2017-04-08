27348f4d3ecea1fd9cc13bf35acb9c909bbe1dee
Muhammad Qureshi <mutahirqureshi@gmail.com>
2017-04-08 00:57:19 -0700

    update folder structure

---
+++ bash/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "bash DIR=$DIR"
+
+function create_link {
+  source="$1"
+  target="$2"
+
+  printf "linking %s to %s..." $source $target
+
+  if [ -e $target ]; then
+    printf "moving already existing %s to %s.bak..." $target $target
+    mv "$target" "${target}.bak"
+  fi
+
+  ln -s $source $target
+  printf "DONE!\n"
+}
+
+for i in "${DIR}"/_*
+do
+  base=$(basename "$i")
+  create_link "$i" "${HOME}/${base/_/.}"
+done
+
---
+++ iterm2/README.md
@@ -0,0 +1,6 @@
+iTerm2
+======
+
+### Setup
+1. import and set color profile to Solarized-Black.itermcolors
+2. report terminal as xterm-256
